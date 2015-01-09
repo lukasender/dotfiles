948e6295999bf355e2e4dcf5cfb3393f984b410b
Lukas Ender <hello@lukasender.at>
2015-01-09 08:59:51 +0100

    brew: updated install process

--- brew/brew.sh
+++ brew/brew.sh
@@ -3,38 +3,27 @@
 
 if [[ "$1" = "--force" ]];
     then
-        echo "about to install brew."
-        #ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-        echo "brew is now installed."
+        if [[ "$2" = "--installBrew" ]];
+        then
+            echo "about to install brew."
+            ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+            echo "brew is now installed."
+        else
+            echo "assuming brew is already installed. installing formulas."
+            echo "(brew isn't installed? use --installBrew as 2. parameter."
+        fi
     else
         echo "you didn't force me to do it!"
         echo "i'm refusing to execute."
         exit 1
 fi
 
-#echo 'time to brew!'
-#
-brew install ack
-brew isntall coreutils
-#brew isntall docker
-brew isntall dsh
-brew isntall findutils
-#brew install git
-#brew isntall libevent
-#brew isntall libmagic
-brew isntall macvim
-#brew isntall nginx
-#brew isntall node
-#brew isntall openssl
-#brew isntall postgresql
-brew isntall pyenv
-#brew isntall readline
-#brew isntall s3cmd
-brew isntall stow
-brew isntall tree
-brew isntall wget
+echo 'time to brew!'
+echo '-------------'
+
+while read line; do brew install "$line"; done < formulas
 
 brew linkapps
 
 # Remove outdated versions from the cellar
-# brew cleanup
+#brew cleanup
