394df5096fa5e344c6027bf6cfbd837f827a5b82
Lukas Ender <hello@lukasender.at>
2015-06-03 15:00:27 +0200

    brew: tweaking; new formulas

--- brew/export_formulas.sh
+++ brew/export_formulas.sh
@@ -3,9 +3,9 @@
 FORMULAS=$1
 if [[ -z "$FORMULAS" ]]
 then
-    echo '-z'
+    echo 'you did not specify a file to export to.'
     FORMULAS=~/sandbox.playground/dotfiles/brew/formulas
+    echo 'I chose' $FORMULAS
 fi
-echo 'nope'
 brew list | sed -e 's/(.*)/(.*)/' > "$FORMULAS"
 brew cask list | sed -e 's/(.*)/(.*)/' > "$FORMULAS-cask"
